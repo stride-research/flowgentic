cf589c0b8a7f34eab2f3eac9c09e25c1950858ee
catebros <caterinabarberoros@gmail.com>
2026-02-04 19:48:17 +0100

    fix: align parslengine with updated baseengine

--- src/flowgentic/backend_engines/parsl.py
+++ src/flowgentic/backend_engines/parsl.py
@@ -1,13 +1,14 @@
 import asyncio
 import parsl
 from parsl.app.app import python_app
-from typing import Callable, Dict, Any
+from typing import Callable, Dict, Any, Optional
 from parsl.config import Config
 from flowgentic.backend_engines.base import BaseEngine
 
 
 class ParslEngine(BaseEngine):
-	def __init__(self, config: Config = None):
+	def __init__(self, config: Config = None, observer: Optional[Callable[[Dict[str, Any]], None]] = None):
+		super().__init__(observer=observer)
 		parsl.load(config)
 
 		self._task_registry = {}
@@ -21,7 +22,7 @@ class ParslEngine(BaseEngine):
 			return python_app(wrapper)
 		return python_app(func)
 
-	async def execute_tool(self, func: Callable, *args, **kwargs) -> Dict[str, Any]:
+	async def execute_tool(self, func: Callable, *args, task_kwargs: Optional[Dict[str, Any]] = None, **kwargs) -> Dict[str, Any]:
 		if func.__name__ not in self._task_registry:
 			self._task_registry[func.__name__] = self._make_parsl_app(func)
 
